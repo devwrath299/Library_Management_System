a6a4587098beae3d7ff9c6950ecb1fcfe146b66d
dev <devwrath1999@gmail.com>
2021-10-26 22:04:41 +0530

    Fragments

--- app/src/main/java/com/example/mylibrary/adminMain.java
+++ app/src/main/java/com/example/mylibrary/adminMain.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,6 +16,11 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.example.mylibrary.Admin_Fragments.Add_Book;
+import com.example.mylibrary.Admin_Fragments.AdminProfile;
+import com.example.mylibrary.Admin_Fragments.Payment;
+import com.example.mylibrary.Admin_Fragments.USers_profile;
+import com.example.mylibrary.Admin_Fragments.Update_book;
 import com.google.android.material.navigation.NavigationView;
 
 public class adminMain extends AppCompatActivity {
@@ -30,18 +36,44 @@ public class adminMain extends AppCompatActivity {
         drawerLayout=findViewById(R.id.admindrawer);
         nav=findViewById(R.id.adminnav);
          setSupportActionBar(toolbar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers,new Add_Book()).commit();
 
         toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment temp;
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                Toast.makeText(getApplicationContext(), "fragement is opening", Toast.LENGTH_SHORT).show();
-                drawerLayout.closeDrawer(GravityCompat.START);
+                switch(item.getItemId())
+                {
+                    case R.id.Addbook:
+                    {
+                        temp=new Add_Book();
+                        break;
+                    }
+                    case R.id.updatebook:
+                    {
+                        temp=new Update_book();
+                        break;
+                    }
+                    case R.id.users_profile:
+                    {
+                        temp= new USers_profile();
+                        break;
+                    }
+                    case R.id.fine:
+                    {
+                        temp=new Payment();
 
+                        break;
+                    }
 
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.containers,temp).commit();
+                 drawerLayout.closeDrawer(GravityCompat.START);
                 return true;
             }
         });
@@ -71,8 +103,9 @@ public class adminMain extends AppCompatActivity {
                 break;
             }
             case R.id.profile:
-                //your action
-                break;
+            {//your action
+                getSupportFragmentManager().beginTransaction().replace(R.id.containers,new AdminProfile()).commit();
+                break;}
             default:
                 return super.onOptionsItemSelected(item);
         }
